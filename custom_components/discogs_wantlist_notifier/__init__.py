45f8205f5a67d8f577a4fe80ce8527f2decb6a6f
JulianDanner <julian@danner-web.de>
2023-05-27 10:55:30 +0200

    add logger output

--- custom_components/discogs_wantlist_notifier/__init__.py
+++ custom_components/discogs_wantlist_notifier/__init__.py
@@ -17,15 +17,17 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the sync service example component."""
     def check_offers_in_wantlist(call: ServiceCall) -> None:
         """Check offers in Discogs Wantlist."""
-        _LOGGER.info('Received data', call.data)
-        
         device = call.data.get("device_name", None)
         token = call.data.get("discogs_token", None)
         min_sleeve_condition = call.data.get("min_sleeve_condition", 'No Cover')
         min_media_condition = call.data.get("min_media_condition", 'G')
+        
+        _LOGGER.info('Received data', call.data)
 
         ## (1) check for good offers
         good_offers, max_price_missing = check_offers_in_wantlist(token, min_media_condition, min_sleeve_condition, interactive=False)
+        
+        _LOGGER.info('offers in wantlist checked')
 
         ## (2) send notifications to 'device' about good offers
 
@@ -44,6 +46,8 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
                   + f'tracklist: {item.tracklist}',
                 url=item.url,
                 device=device)
+        
+        _LOGGER.info('notifications on offers sent')
 
         if len(max_price_missing) > 0:
             send_notification(
@@ -51,6 +55,8 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 msg=f'Please set a max-price for the following items: {max_price_missing}',
                 url=item.url,
                 device=device)
+        
+        _LOGGER.info('notifications on offers sent')
 
     # Register our service with Home Assistant.
     hass.services.register(DOMAIN, 'check_offers_in_wantlist', check_offers_in_wantlist)
