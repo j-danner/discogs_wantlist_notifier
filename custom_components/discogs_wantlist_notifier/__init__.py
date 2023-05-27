77f3f2caace6daf5235d127277e5ab79e3c7c38f
JulianDanner <julian@danner-web.de>
2023-05-27 13:27:03 +0200

    fixing bugs...

--- custom_components/discogs_wantlist_notifier/__init__.py
+++ custom_components/discogs_wantlist_notifier/__init__.py
@@ -15,37 +15,34 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the sync service example component."""
-    def check_offers_in_wantlist(call: ServiceCall) -> None:
+    def check_offers_in_wantlist_service(call: ServiceCall) -> None:
         """Check offers in Discogs Wantlist."""
-        device = call.data.get("device_name", None)
-        token = call.data.get("discogs_token", None)
-        min_sleeve_condition = call.data.get("min_sleeve_condition", 'No Cover')
-        min_media_condition = call.data.get("min_media_condition", 'G')
-        
-        _LOGGER.info('Received data', call.data)
-
-        ## (1) check for good offers
-        good_offers, max_price_missing = check_offers_in_wantlist(token, min_media_condition, min_sleeve_condition, interactive=False)
-        
-        _LOGGER.info('offers in wantlist checked')
-
-        ## (2) send notifications to 'device' about good offers
 
         #helper function to send notifications to given device, clicking on notification opens given url
         def send_notification(title:str, msg:str, url:str, device:str) -> None:
             service_data = {"message": msg, "title": title, "data": {"notification_icon": "mdi:album", "group": "Discogs Wantlist Watcher", "color": "black", "clickAction": url}}
             hass.services.call('notify', f'mobile_app_{device}', service_data, False)
         
+        device = call.data.get("device_name", None)
+        token = call.data.get("discogs_token", None)
+        min_sleeve_condition = Condition( call.data.get("min_sleeve_condition", 'No Cover') )
+        min_media_condition = Condition( call.data.get("min_media_condition", 'G') )
+        
+        _LOGGER.info('Received data')
+
+        ## (1) check for good offers
+        good_offers, max_price_missing = check_offers_in_wantlist(token, min_media_condition, min_sleeve_condition)
+        
+        _LOGGER.info('offers in wantlist checked')
+
+        ## (2) send notifications to 'device' about good offers
+
         for offer in good_offers:
             item = offer['wantlist_item'].release
-            send_notification(
-                title=f'Good offer found for {item.title}!',
-                msg=f'media condition: {offer["media_condition"]}, sleeve condition: {offer["sleeve_condition"]}; ' 
-                  + f'price {offer["price"]} (max-price: {parse_price(offer["wantlist_item"])})\n'
-                  + f' marketplace stats: {get_price_stats(item.id, url=item.url)}\n'
-                  + f'tracklist: {item.tracklist}',
-                url=item.url,
-                device=device)
+            title = f'Good offer found for {item.title} -- {item.tracklist}',
+            msg = f'price {offer["price"]} (max-price: {parse_price(offer["wantlist_item"])})\n' + f'media condition: {offer["media_condition"]}, sleeve condition: {offer["sleeve_condition"]}\n' + f'marketplace stats: {get_price_stats(item.id, url=item.url)}\n'
+            _LOGGER.info(title + ' -- ' + msg)
+            send_notification(title=title, msg=msg, url=item.url, device=device)
         
         _LOGGER.info('notifications on offers sent')
 
@@ -56,10 +53,10 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 url=item.url,
                 device=device)
         
-        _LOGGER.info('notifications on offers sent')
+        _LOGGER.info('notifications on missing pricemissing prices sent')
 
     # Register our service with Home Assistant.
-    hass.services.register(DOMAIN, 'check_offers_in_wantlist', check_offers_in_wantlist)
+    hass.services.register(DOMAIN, 'check_offers_in_wantlist', check_offers_in_wantlist_service)
 
     # Return boolean to indicate that initialization was successfully.
     return True
