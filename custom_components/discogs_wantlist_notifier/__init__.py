f96767e1cf09a1c2fa30aedfe2fbb36d6ba3b8d1
JulianDanner <julian@danner-web.de>
2023-05-27 10:42:14 +0200

    replace test service call with actual API call

--- custom_components/discogs_wantlist_notifier/__init__.py
+++ custom_components/discogs_wantlist_notifier/__init__.py
@@ -25,7 +25,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
         min_media_condition = call.data.get("min_media_condition", 'G')
 
         ## (1) check for good offers
-        #good_offers, max_price_missing = check_offers_in_wantlist(token, min_media_condition, min_sleeve_condition, interactive=False)
+        good_offers, max_price_missing = check_offers_in_wantlist(token, min_media_condition, min_sleeve_condition, interactive=False)
 
         ## (2) send notifications to 'device' about good offers
 
@@ -34,26 +34,23 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
             service_data = {"message": msg, "title": title, "data": {"notification_icon": "mdi:album", "group": "Discogs Wantlist Watcher", "color": "black", "clickAction": url}}
             hass.services.call('notify', f'mobile_app_{device}', service_data, False)
         
+        for offer in good_offers:
+            item = offer['wantlist_item'].release
+            send_notification(
+                title=f'Good offer found for {item.title}!',
+                msg=f'media condition: {offer["media_condition"]}, sleeve condition: {offer["sleeve_condition"]}; ' 
+                  + f'price {offer["price"]} (max-price: {parse_price(offer["wantlist_item"])})\n'
+                  + f' marketplace stats: {get_price_stats(item.id, url=item.url)}\n'
+                  + f'tracklist: {item.tracklist}',
+                url=item.url,
+                device=device)
 
-        send_notification(f'Test Title!', f'Test message! (media condition: {min_media_condition}, sleeve condition: {min_sleeve_condition})', "https://www.discogs.com/release/484125-Men-At-Work-Down-Under?ev=drec", device)
-
-        #for offer in good_offers:
-        #    item = offer['wantlist_item'].release
-        #    send_notification(
-        #        title=f'Good offer found for {item.title}!',
-        #        msg=f'media condition: {offer["media_condition"]}, sleeve condition: {offer["sleeve_condition"]}; ' 
-        #          + f'price {offer["price"]} (max-price: {parse_price(offer["wantlist_item"])})\n'
-        #          + f' marketplace stats: {get_price_stats(item.id, url=item.url)}\n'
-        #          + f'tracklist: {item.tracklist}',
-        #        url=item.url,
-        #        device=device)
-
-        #if len(max_price_missing) > 0:
-        #    send_notification(
-        #        title=f'Found items without max price!',
-        #        msg=f'Please set a max-price for the following items: {max_price_missing}',
-        #        url=item.url,
-        #        device=device)
+        if len(max_price_missing) > 0:
+            send_notification(
+                title=f'Found items without max price!',
+                msg=f'Please set a max-price for the following items: {max_price_missing}',
+                url=item.url,
+                device=device)
 
     # Register our service with Home Assistant.
     hass.services.register(DOMAIN, 'check_offers_in_wantlist', check_offers_in_wantlist)
